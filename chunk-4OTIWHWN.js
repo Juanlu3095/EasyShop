e64501ad302db6429f0df64b051e56a12646d022
Juanlu3095 <85942298+Juanlu3095@users.noreply.github.com>
2025-02-03 19:34:13 +0000

    Deploying to gh-pages from  - 77cae44e26550fc3d08ed5be01b82dd0208177c1 🚀

---
+++ chunk-4OTIWHWN.js
@@ -0,0 +1,1 @@
+var o={production:!0,ApiEndpoint:"https://easyshopapi-l1sx.onrender.com/api",BackEndPoint:"https://easyshopapi-l1sx.onrender.com",FilesEndpoint:"https://easyshopapi-l1sx.onrender.com/storage/",DriveEndpoint:"https://drive.google.com/thumbnail?id=",DriveDownloadEndpoint:"https://drive.google.com/file/d/",BaseUrl:"/EasyShop"};export{o as a};
